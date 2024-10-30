983dd46e288d50eaf77bd44f8905fad1588d06b7
mik-305 <mik-305@bk.ru>
2024-10-30 17:49:47 +0300

    Update module_9_2.py

--- module_9_2.py
+++ module_9_2.py
@@ -3,7 +3,7 @@ second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler
 
 first_result = [len(x) for x in first_strings if len(x) > 5]
 print(first_result)
-second_result = [x +','+ y for x in first_strings for y in second_strings if len(x) == len(y)]
-print(second_result)
-third_result = [x+':'+str(len(x)) for x in first_strings + second_strings if len(x) % 2 == 0]
+second_result = [x +',' + y for x in first_strings for y in second_strings if len(x) == len(y)]
+print(f"{second_result}")
+third_result = {x+':'+str(len(x)) for x in first_strings + second_strings if len(x) % 2 == 0}
 print(third_result)
